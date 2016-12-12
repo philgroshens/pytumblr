9fc5119cf9672d364a4be6288260e94766f83b30
Phil Groshens <mail@philgroshens.me>
2016-12-12 16:00:31 -0600

    updated

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ from setuptools import setup
 setup(
 
     name="PyTumblr",
-    version="0.0.6.py3",
+    version="0.0.7",
     description="A Python API v2 wrapper for Tumblr (updated for Python3 support)",
-    author="John Bunting (Python3 changes by github.com/philgroshens)",
-    author_email="johnb@tumblr.com",
-    url="https://github.com/tumblr/pytumblr",
+    author="Phil Groshens (Original wrapper by John Bunting (johnb@tumblr.com))",
+    author_email="offerhero@gmail.com",
+    url="https://github.com/philgroshens/pytumblr",
     packages = ['pytumblr'],
     license = "LICENSE",
 
